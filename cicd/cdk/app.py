51c405caacec38afcc3e585d74ce311320354d6a
Petros Georgiadis <>
2023-05-11 10:16:53 +0200

    Config update

--- cicd/cdk/app.py
+++ cicd/cdk/app.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-import os
-
+import json
 import aws_cdk as cdk
 
 from cdk.cicd_stack import CiCdStack
@@ -9,7 +8,9 @@ from cdk.cicd_stack import CiCdStack
 app = cdk.App()
 
 
-conf = app.node.try_get_context(key="config")
+with open('../config/config.json', 'r') as f:
+    conf = json.load(f)
+
 env = cdk.Environment(account=conf["aws_account"], region=conf["aws_region"])
 
 CiCdStack(app, conf["prefix"]+"CiCdStack",
